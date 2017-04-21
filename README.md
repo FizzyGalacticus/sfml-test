3eb277098b444fb055b776e29e2f50260dd6f9d5
Dustin L. Dodson <dustinleedodson@gmail.com>
2017-04-20 19:53:13 -0800

    Added bits to cycle through background music and added first track. Also added README to keep track of attributions.

---
+++ README.md
@@ -0,0 +1,7 @@
+#SFML-test
+
+This is just a screw-around project to help me learn some SFML and general game development.
+
+##Attributions
+###Audio
+snd_background01.wav - Remix of Hans Atom's [Who We Are](http://dig.ccmixter.org/files/hansatom/55486)
--- soundhandler.cpp
+++ soundhandler.cpp
@@ -6,16 +6,16 @@ using std::endl;
 
 SoundHandler::SoundHandler() :
 _backgroundTracks{
-	
+	"audio/snd_background01.wav"
 },
 _currentBackgroundTrack(0) {
 	if(!this->_backgroundTracks.size()) {
 		if(this->getDebugging())
 			cout << "No audio tracks given for background music." << endl;
 	}
-	else if(!this->_backgroundMusic.openFromFile(this->_backgroundTracks[0])) {
+	else if(!this->_backgroundMusic.openFromFile(this->_backgroundTracks[this->_currentBackgroundTrack])) {
 		if(this->getDebugging())
-			cout << "Could not open background track: " << this->_backgroundTracks[0] << endl;
+			cout << "Could not open background track: " << this->_backgroundTracks[this->_currentBackgroundTrack] << endl;
 	}
 	else {
 		this->_backgroundMusic.setVolume(50);
@@ -28,5 +28,9 @@ SoundHandler::~SoundHandler() {
 }
 
 void SoundHandler::update(RenderWindow * window, sf::Time clock) {
-
+	if(this->_backgroundMusic.getStatus() == sf::SoundStream::Stopped) {
+		this->_currentBackgroundTrack = (this->_currentBackgroundTrack < this->_backgroundTracks.size()-1 ? this->_currentBackgroundTrack+1:0);
+		this->_backgroundMusic.openFromFile(this->_backgroundTracks[this->_currentBackgroundTrack]);
+		this->_backgroundMusic.play();
+	}
 }
